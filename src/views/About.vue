7aa267b3f48e4ecd1bea1ff8f637f4bc48bccf91
Eric Mason <m.mason@wustl.edu>
2018-07-05 14:08:40 -0500

    skeleton panel list

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>List of Panels</h1>
+    <Panels/>
   </div>
 </template>
+
+
+<script>
+// @ is an alias to /src
+import Panels from "@/components/Panels.vue";
+
+export default {
+  name: "about",
+  components: {
+    Panels
+  }
+};
+</script>
