edd154eaab75a6b8bb8c374caf18405b7a32683c
sorayamoss <sorayamoss@wustl.edu>
2018-07-12 13:53:07 -0500

    made css changes, added flex

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,21 @@
 <template>
   <div>
     <div class="menu">
-      <img class="logo" src="../assets/slalom-logo-blue-RGB.jpg"/>
-      <i class="fas fa-bars fa-2x" v-on:click="show = !show"></i>
+      <!-- <img class="logo" src="../assets/slalom-logo-blue-RGB.jpg"/> -->
+      <span class="logo">HealthSTLx <span class="logoLogo"><i class="fas fa-medkit"></i></span></span>
+      <span class="menuLogo"><i class="fas fa-bars fa-2x" v-on:click="show = !show"></i></span>
     </div>
 
     <transition name="fade">
         <div class="overlay" v-if="show" v-on:click="show = !show">
         <i class="fas fa-times fa-2x"></i>
 	         <div class="wrap">
-		          <ul class="wrap-nav">
-                <li><router-link to="people">Speakers</router-link></li>
-				        <li><router-link to="about">Breakout Sessions</router-link></li>
-                <li><router-link to="schedule">Schedule</router-link></li>
-                <li><router-link to="registration">Register</router-link></li>
-                <li><router-link to="sponsor">Sponsors</router-link></li>
-                <li><router-link to="/">About</router-link></li>
-		          </ul>
+              <router-link to="people">Speakers</router-link>
+			        <router-link to="about">Breakout Sessions</router-link>
+              <router-link to="schedule">Schedule</router-link>
+              <router-link to="registration">Register</router-link>
+              <router-link to="sponsor">Sponsors</router-link>
+              <router-link to="/">About</router-link>
 		       </div>
 	      </div>
     </transition>
@@ -42,19 +41,21 @@ export default {
 
 <style>
 .menu {
+  display:flex;
+  align-items: center;
   position: fixed;
   background-color: #fff;
   top: 0;
   z-index: 10;
   width: 100%;
+  box-shadow: 0px 1px 4px grey;
+  justify-content: space-between; 
 }
-.menu i {
-  position: absolute;
-  right: 0;
-  padding-right: 50px;
-  padding-top: 40px;
+.menu .menuLogo {
+  float: right;
+  padding: 20px;
 }
-.menu i:hover {
+.menu .menuLogo:hover {
   color: #e5e5e5;
   cursor: pointer;
 }
@@ -62,18 +63,22 @@ export default {
   color: #e5e5e5;
   position: absolute;
   right: 0;
-  padding-right: 50px;
-  padding-top: 40px;
+  margin-right: 50px;
+  margin-top: 40px;
   cursor: pointer;
 }
 .overlay i:hover{
   color:#FFE72C;
 }
+.logoLogo {
+  display: inline;
+}
 .logo {
-  float: left;
-  width: 30px;
-  padding: 10px;
-
+  display: flex;
+  width: 300px;
+  font-weight: bold;
+  font-size: 35px;
+  color: #0040a8;
 }
 ul {
 	list-style:none;
@@ -82,49 +87,36 @@ ul {
 .overlay {
 	position:fixed;
 	top:0;
-	height:100%;
-	width:100%;
+	height:100vh;
+	width:100vw;
 	background:#333;
 	overflow:auto;
 	z-index:99;
 }
 .wrap {
 	color:#e9e9e9;
-	text-align:center;
-	max-width:90%;
-	margin:0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+	/* text-align:center; */
+	/* max-width:90%; */
+	/* margin:0 auto; */
 }
 .wrap ul.wrap-nav {
-	padding:15%;
+	/* padding:15%; */
 }
-li a {
+a {
   list-style:none;
 	color:#34B484;
 	padding:8px 0;
 	text-decoration:none;
   font-size:30px;
-  display:inline-block;
-  vertical-align:top;
-  position:relative;
   color:#f0f0f0;
 }
-
-.wrap ul.wrap-nav li a:hover {
+ a:hover {
 	color:#FFE72C;
-}
-@media screen and (max-width:48em) {
-	.wrap ul.wrap-nav>li {
-		width:100%;
-		padding:20px 0;
-		border-bottom:1px solid #575757;
-	}
-	.wrap ul.wrap-nav {
-		padding:30px 0px 0px;
-	}
-	nav ul {
-		opacity:0;
-		visibility:hidden;
-	}
 }
 .fade-enter-active, .fade-leave-active {
   transition: opacity .5s;
