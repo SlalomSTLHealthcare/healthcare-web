a5198d2f3acacee845f2d933340e691e03a4be7f
sorayamoss <sorayamoss@wustl.edu>
2018-07-13 10:56:55 -0500

    functioning ranking breakouts

--- src/components/SelectBreakout.vue
+++ src/components/SelectBreakout.vue
@@ -3,9 +3,9 @@
     <p>{{timeSlot}}</p>
     <el-transfer
     :titles="['All Breakouts', 'Ranked Breakouts']"
-    :target-order="push"
+    :target-order="'push'"
     v-model="value2"
-    :data="data2">
+    :data="computedData">
       <!-- v-model="value"
       :data="data"> -->
     </el-transfer>
@@ -18,85 +18,31 @@ import _ from 'underscore';
 export default {
   name: "SelectBreakout",
   data() {
-  const generateData2 = _ => {
-    const data = [];
-    const breakouts = ['Chronic Disease/High Flier', 'Healthy People and End of Life', 'Acute Care', 'Development ', 'Data for Health/Caring for the Whole Patien', 'Security of Data', 'Research ', 'Partnerships/M&A/Business Models'];
-    // const initials = ['CA', 'IL', 'MD', 'TX', 'FL', 'CO', 'CT'];
-    breakouts.forEach((city, index) => {
-      data.push({
-        label: city,
-        key: index,
-        // initial: initials[index]
-      });
-    });
-    return data;
-  };
-  return {
-    data2: generateData2(),
-    value2: [],
-    filterMethod(query, item) {
-      return item.initial.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    return {
+      breakoutData: [],
+      value2: []
     }
-  };
-},
-//   data() {
-//     const generateData = _ => {
-//       const data=[];
-//       const breakouts = ['Chronic Disease/High Flier', 'Healthy People and End of Life', 'Acute Care', 'Development ', 'Data for Health/Caring for the Whole Patien', 'Security of Data', 'Research ', 'Partnerships/M&A/Business Models'];
-//       breakouts.forEach((breakout, index) => {
-//         data.push({
-//           label: breakout,
-//           key: index
-//         });
-//       });
-//       return data;
-//     };
-//   return {
-//     data: generateData(),
-//     value: []
-//   }
-// },
-props: {
-  timeSlot: String
-}
-// computed:{
-//   computedData: function(){
-//     console.log(_.filter(this.breakoutData, s => s.session_type === "Breakout"));
-//     return _.filter(this.breakoutData, s => s.session_type === "Breakout");
-//     console.log(this.data);
-//     this.data.forEach(function(element) {
-//       console.log("heloooooo");
-//       if(element.session_type==="Breakout"){
-//         let i=0;
-//         this.breakoutData.push({
-//           key: i,
-//           label: element.title
-//         });
-//         console.log("successful push");
-//       }
-//     })
-//   }
-// },
-// mounted: function() {
-//   axios.get(`https://slalom-health-api-staging.herokuapp.com/api/sessions`)
-//    .then(response => {
-//      this.data=response.data;
-//      this.data.forEach(function(element) {
-//        console.log("heloooooo");
-//        if(element.session_type==="Breakout"){
-//          let i=0;
-//          this.breakoutData.push({
-//            key: i,
-//            label: element.title
-//          });
-//          console.log("successful push");
-//        }
-//      })
-//   })
-//    .catch(e => {
-//      console.log("error")
-//    })
-// }
+  },
+  mounted: function() {
+    axios.get(`https://slalom-health-api-staging.herokuapp.com/api/sessions`)
+     .then(response => {
+       this.breakoutData = response.data
+    })
+     .catch(e => {
+       console.log("error")
+     })
+  },
+  props: {
+    timeSlot: String
+  },
+  computed:{
+    computedData() {
+      return _.map(_.filter(this.breakoutData, s => s.session_type === 'Breakout'), s => ({
+        key: s.id,
+        label: s.title
+      }))
+    }
+  }
 };
 </script>
 
