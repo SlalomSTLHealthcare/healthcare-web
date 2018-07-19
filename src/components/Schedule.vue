da79a69c11241ad6fbe480c787bb8db1952f4a05
Eric Mason <m.mason@wustl.edu>
2018-07-19 11:46:03 -0500

    schedule-expansion-addition

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -3,15 +3,27 @@
   <h1>Schedule</h1>
   <el-table
         :data="scheduleData"
-        border
+        ref="scheduleTable"
         stripe
-        class="events"
         :default-sort = "{prop:'start_time', order: 'ascending'}"
-        style="width: 100%">
+        :row-class-name="tableRowSetter"
+        @row-click="changeExpansion"
+        @expand-change="setCurrentExpandedRow"
+        style="width: 100%"
+        >
         <el-table-column
           prop="start_time"
           width="90"
+          class="test"
           :formatter="timeFormatter">
+        </el-table-column>
+        <el-table-column
+          width="50"
+          type="expand">
+          <template slot-scope="props">
+            <p v-for="session in getSessions(props.row)" v-if="session.session_type == 'Extra'">{{session.description}}, Room Number: {{session.room_num}}</p>
+            <p v-else>{{session.title}}, Room Number: {{session.room_num}}</p>
+          </template>
         </el-table-column>
         <el-table-column
           prop="title"
@@ -32,23 +44,68 @@ export default {
   },
   data () {
     return{
-      scheduleData: []
+      scheduleData: [],
+      sessions: [],
+      currentExpanded: []
     }
   },
   methods: {
+    changeExpansion(val){
+      console.log(val);
+      var isFound = false;
+      this.currentExpanded.forEach(function(row){
+        if(row === val){
+          console.log("match");
+          isFound = true;
+        }
+      });
+      if(isFound){
+        this.$refs.scheduleTable.toggleRowExpansion(val,false);
+      }else{
+        this.$refs.scheduleTable.toggleRowExpansion(val,true);
+      }
+
+    },
+    setCurrentExpandedRow(val,expandedRows){
+      this.currentExpanded = expandedRows;
+    },
+    tableRowSetter(){
+      return 'tableRow';
+    },
     populateData: function() {
       this.$axiosServer.get(`api/schedules`)
       .then(response => {
         // JSON responses are automatically parsed.
         this.scheduleData = response.data;
+
       })
       .catch(e => {
         console.log("error");
         return e;
+      });
+
+      this.$axiosServer.get(`api/sessions`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.sessions = response.data;
       })
+      .catch(e => {
+        console.log("error");
+        return e;
+      });
+
     },
     timeFormatter: function(row,column) {
       return moment(row.start_time).format("h:mm A");
+    },
+    getSessions(schedule){
+      var attachedSessions = [];
+      this.sessions.forEach(function(element){
+        if(element.schedule_id === schedule.id){
+          attachedSessions.push(element);
+        }
+      });
+      return attachedSessions;
     }
   }
 };
@@ -56,15 +113,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-.events{
+
+.el-table{
   text-align: center;
+  cursor: pointer;
 }
+
 .session{
   display: block;
   position: relative;
 }
 label{
   font-weight: normal;
+}
+p{
+  text-align: left;
 }
 h1 {
   text-align: center;
