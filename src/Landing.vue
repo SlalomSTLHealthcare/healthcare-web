9c1f053d1b72f89fed245a776919a696a7c89864
Daniel Borstelmann <dborstelmann@wustl.edu>
2018-07-22 21:43:11 -0400

    sponsor + footer

--- src/Landing.vue
+++ src/Landing.vue
@@ -1,71 +1,23 @@
 <template>
     <div id="landing">
-        <!-- <div class="nav-header">
-          <el-row>
-              <div class="nav-header">
-                  <div class="left-header">
-                      <img style="height: 50px;" src="./assets/final-logo.png">
-                      <span style="color: #333; font-weight: 300; font-size: 1.5rem; margin-left: 14px;">health</span>
-                      <span style="color: #333; font-weight: 700; font-size: 1.5rem;">STLX</span>
-                  </div>
-                  <div class="right-header">
-                      <span>Presented By</span>
-                      <img style="width: 200px;" src="./assets/slalom-logo-blue-RGB.jpg">
-                  </div>
-              </div>
-          </el-row>
-        </div>
-        <div class="banner">
-            <div class="title">
-                HealthSTLX
-            </div>
-            <div class="subtitle">
-                Let's change the world
-            </div>
-            <div class="date">
-                October 24, 2018
-            </div>
-            <div class="location">
-                St. Louis, MO
-            </div>
-        </div>
-        <div>
-          SPONSORS
-        </div>
-        <div class="nav-footer">
-            <div class="contact-column site-links">
-                <div class="title">
-                    HealthSTLX
-                </div>
-            </div>
-            <div class="contact-column contact-us">
-                <div class="title">
-                    Contact
-                </div>
-                <span>tara@slalom.com</span>
-                <div class="social-links">
-                    <a target="_blank" href="https://www.facebook.com/liquidsplit/"><i class="fa fa-facebook"></i></a>
-                    <a target="_blank" href="https://twitter.com/Liquid_Split"><i class="fa fa-twitter"></i></a>
-                    <a target="_blank" href="https://instagram.com/liquidsplitinc"><i class="fa fa-instagram"></i></a>
-                </div>
-            </div>
-        </div> -->
-        <div class="section">
-            <div class="central-box-wrapper">
-                <div class="central-box">
-                    <div class="logo-section">
-                        <img class="logo-img" src="./assets/final-logo.png">
-                    </div>
-                    <div class="content-section">
-                        <div class="date">
-                            10.24.18
+        <div class="first-section">
+            <div class="sticky-helper">
+                <div class="central-box-wrapper">
+                    <div class="central-box">
+                        <div class="logo-section">
+                            <img class="logo-img" src="./assets/final-logo.png">
                         </div>
-                        <div class="bar"></div>
-                        <div class="location">
-                            Eric P. Newman Education Center
-                        </div>
-                        <div class="location-bottom">
-                            St. Louis, MO
+                        <div class="content-section">
+                            <div class="date">
+                                11.14.18
+                            </div>
+                            <div class="bar"></div>
+                            <div class="location">
+                                Eric P. Newman Education Center
+                            </div>
+                            <div class="location-bottom">
+                                St. Louis, MO
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -80,15 +32,28 @@
                 </div>
             </div>
         </div>
-        <div class="section">
-            <div class="central-box-wrapper">
-                <div class="central-box">
-                    <div class="sponsor-section">
-                        <div class="title">
-                            How to become a sponsor
-                        </div>
-                    </div>
-                </div>
+        <div class="sponsor-section">
+            <div class="title">
+                sponsor this event
+            </div>
+            <div class="bar"></div>
+            <div class="subtitle">
+                How to become a sponsor
+            </div>
+            <div class="text">
+                This is some text that will get people to join as a sponsor.
+            </div>
+            <div class="action">
+                <el-button type="primary" class="sponsor-button" plain round>Become a sponsor</el-button>
+            </div>
+        </div>
+        <div class="footer-section">
+            <div class="pusher"></div>
+            <div class="slalom"><img style="height: 20px;" src="./assets/slalom-white.png"></div>
+            <div class="social">
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-linkedin-in"></i>
             </div>
         </div>
     </div>
@@ -104,9 +69,6 @@ export default {
     body {
         font-family: 'Roboto', sans-serif;
         margin: 0;
-        background: url('./assets/background-landing.jpg') no-repeat center center fixed;
-        background-size: cover;
-        background-attachment: fixed;
     }
 
     @secondary-light: #FB0ECE;
@@ -116,18 +78,97 @@ export default {
     @primary: #1224ff;
     @primary-dark: #000;
 
-    .section {
+    @sponsor-purple: #651986;
+    @sponsor-purple-dark: #420E5D;
+
+    .footer-section {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #000;
+        color: #fff;
+        height: 60px;
+        text-align: center;
+
+        div {
+            width: 150px;
+        }
+
+        .social {
+            i {
+                cursor: pointer;
+                font-size: 1.5rem;
+                margin: 0 8px;
+            }
+        }
+    }
+
+    .sponsor-section {
+        padding: 0 20px;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background: linear-gradient(@sponsor-purple-dark, @sponsor-purple);
+        color: #fff;
+        letter-spacing: 0.2px;
+
+        .title {
+            margin-top: 4rem;
+            font-size: 2rem;
+            font-weight: 100;
+            text-transform: uppercase;
+        }
+
+        .bar {
+            margin-top: 2.5rem;
+            height: 5px;
+            width: 100px;
+            background-color: #fff;
+        }
+
+        .subtitle {
+            font-weight: 300;
+            font-size: 2rem;
+            margin-top: 4rem;
+        }
+
+        .text {
+            font-size: 0.9rem;
+            font-weight: 300;
+            margin-top: 2rem;
+        }
+
+        .action {
+            margin: 6rem 0 4rem;
+
+            .sponsor-button {
+                border-color: #fff;
+                background-color: transparent;
+                color: #fff;
+
+                &:hover {
+                    background-color: #fff;
+                    color: @sponsor-purple;
+                }
+            }
+        }
+    }
+
+    .first-section {
         min-height: 100vh;
         width: 100vw;
         color: #fff;
-        display: flex;
-        justify-content: center;
-        align-items: center;
+        background: url('./assets/background-landing.jpg') no-repeat center center fixed;
+        background-size: cover;
 
         .floated-content-section {
-            position: fixed;
-            top: 16vh;
-            left: 22vw;
+            position: sticky;
+            top: 11.5vh;
+            padding-bottom: 20px;
+            margin-left: 22vw;
 
             .title {
                 font-size: 5rem;
@@ -143,6 +184,7 @@ export default {
             }
 
             .subtitle {
+                letter-spacing: 0.2px;
                 font-weight: 100;
                 margin-top: 16px;
                 max-width: 13vw;
@@ -150,77 +192,86 @@ export default {
             }
         }
 
-        .central-box-wrapper {
-            padding: 10px;
-            border: 10px solid @secondary;
-            border-image-source: linear-gradient(45deg, darken(@secondary, 33%), @secondary-light, darken(@secondary-light, 33%));
-            border-image-slice: 10;
-            box-shadow: 0 0 .1rem .05rem fade(@secondary-dark, 20%);
-            text-align: center;
+        .sticky-helper {
+            position: absolute;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 100vw;
+            height: 100vh;
 
-            .central-box {
-                padding: 40px 0 20px;
-                width: 24vw;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
+            .central-box-wrapper {
+                padding: 10px;
+                border: 10px solid @secondary;
+                border-image-source: linear-gradient(45deg, darken(@secondary, 33%), @secondary-light, darken(@secondary-light, 33%));
+                border-image-slice: 10;
+                box-shadow: 0 0 .1rem .05rem fade(@secondary-dark, 20%);
+                text-align: center;
 
-                .logo-section {
-                    width: 70%;
-
-                    .logo-img {
-                        width: 100%;
-                        filter: drop-shadow(0 0 2rem lighten(@secondary-dark, 38%));
-                    }
-                }
-
-                .content-section {
+                .central-box {
+                    padding: 40px 0 20px;
+                    width: 24vw;
                     display: flex;
                     flex-direction: column;
                     justify-content: center;
                     align-items: center;
-                    letter-spacing: 1px;
-                    text-shadow: .2rem .2rem .3rem fade(@secondary-dark, 20%);
 
-                    .date {
-                        margin: 16px 0 10px 0;
-                        font-weight: 700;
-                        font-size: 1.5rem;
+                    .logo-section {
+                        width: 70%;
+
+                        .logo-img {
+                            width: 100%;
+                            filter: drop-shadow(0 0 2rem lighten(@secondary-dark, 38%));
+                        }
                     }
 
-                    .bar {
-                        margin-bottom: 8px;
-                        height: 2px;
-                        width: 70px;
-                        background-color: #fff;
+                    .content-section {
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: center;
+                        align-items: center;
+                        letter-spacing: 1px;
+                        text-shadow: .2rem .2rem .3rem fade(@secondary-dark, 20%);
+
+                        .date {
+                            margin: 16px 0 10px 0;
+                            font-weight: 700;
+                            font-size: 1.5rem;
+                        }
+
+                        .bar {
+                            margin-bottom: 8px;
+                            height: 2px;
+                            width: 70px;
+                            background-color: #fff;
+                        }
+
+                        .location {
+                            margin-top: 8px;
+                            font-weight: 500;
+                            letter-spacing: 0.5px;
+                            font-size: 0.85rem;
+                        }
+
+                        .location-bottom {
+                            margin-top: 2px;
+                            font-weight: 500;
+                            letter-spacing: 0.5px;
+                            font-size: 0.85rem;
+                        }
                     }
 
-                    .location {
-                        margin-top: 8px;
-                        font-weight: 500;
-                        letter-spacing: 0.5px;
-                        font-size: 0.85rem;
-                    }
+                    .sponsor-section {
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: center;
+                        align-items: center;
+                        letter-spacing: 1px;
 
-                    .location-bottom {
-                        margin-top: 2px;
-                        font-weight: 500;
-                        letter-spacing: 0.5px;
-                        font-size: 0.85rem;
-                    }
-                }
-
-                .sponsor-section {
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                    letter-spacing: 1px;
-
-                    .title {
-                        font-weight: 100;
-                        font-size: 1.5rem;
+                        .title {
+                            font-weight: 100;
+                            font-size: 1.5rem;
+                        }
                     }
                 }
             }
@@ -240,6 +291,7 @@ export default {
             width: 100%;
             text-align: center;
             position: absolute !important;
+            margin: 0 !important;
 
             .title {
                 font-size: 4rem !important;
@@ -299,6 +351,10 @@ export default {
 
         .central-box {
             width: 70vw !important;
+        }
+
+        .pusher {
+            display: none;
         }
     }
 
