8a19557f78382d9fbbfbcc646a8f7c5bd5c28ae2
Taylor Jackson <tjtay2008@gmail.com>
2018-08-03 15:35:16 -0500

    updated and responsive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,15 @@
 <template>
   <div class="home">
-    <HomeOverlay class="above-fold"/>
-    <HomeSponsor/>
-    <!-- <Footer/> -->
+    <HomePage/>
   </div>
 </template>
 
 <script>
-import Login from "@/components//Login.vue";
-import HomeOverlay from "@/components/HomeOverlay.vue";
-import HomeSponsor from "@/components/HomeSponsor.vue";
-import NavBar from "@/components/NavBar.vue";
-import Footer from "@/components/Footer.vue";
-
+import HomePage from "@/components/HomePage.vue";
 export default {
   name: "home",
   components: {
-    HomeOverlay,
-    HomeSponsor,
-    NavBar,
-    Footer,
-    Login
-  },
-  methods: {
-    about: function() {
-      this.$router.push("/about");
-    }
+    HomePage
   }
 };
 </script>
