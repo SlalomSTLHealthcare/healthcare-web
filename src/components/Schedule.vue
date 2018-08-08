d0352ec4f42773328fb355e92a857800deda6fdd
Taylor Jackson <tjtay2008@gmail.com>
2018-08-08 15:04:04 -0500

    small changes

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -4,7 +4,7 @@
     <div class="decoration"></div>
     <table>
       <tr v-for="event in scheduleData" valign="top">
-        <td style="" class="time">
+        <td class="time">
           <h2 class="timeHeader">{{timeFormatter(event)}}</h2>
           <p class="roomNum" v-for="session in getSessions(event)" >Room #{{session.room_num}}</p>
         </td>
@@ -78,7 +78,6 @@ export default {
   }
 };
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
 
@@ -89,7 +88,7 @@ div{
     border-right: 1px solid #b5bfbd;
     color: #b5bfbd;
     padding-right: 10px;
-    text-align: right
+    text-align: right;
 }
 .scheduleEvent{
   padding-left: 25px;
@@ -98,8 +97,8 @@ div{
 table{
   border-spacing: 0;
   background-color: white;
-  margin: 3% auto;
-  padding: 5% 15%;
+  margin: 4% 5.3%;
+  padding: 5% 20%;
 }
 .eventHeader{
   color: #162565;
@@ -110,15 +109,15 @@ table{
 h1 {
   font-size: 40px;
   font-weight: lighter;
-  margin-left: 10%;
-  margin-top: 100px;
+  margin-left: 5.3%;
+  margin-bottom: 16px;
 }
 
 .decoration {
   background-color: #005aed;;
   height: 5px;
   width: 75px;
-  margin-left: 10%;
+  margin-left: 5.3%;
 }
 
 .roomNum, .timeHeader, .event, .eventHeader {
@@ -128,7 +127,6 @@ h1 {
   padding-bottom: 3px;
 }
 @media only screen and (max-width:349px){
-
     h2 {
         font-size: 15px;
     }
@@ -138,6 +136,12 @@ h1 {
       padding-left: 15px;
       padding-right: 10px;
       }
+    table{
+      border-spacing: 0;
+      background-color: white;
+      margin: 4% 0%;
+      padding: 5% 0%;
+    }
     .time{
       padding-top:0;
       padding-bottom: 0;
@@ -145,6 +149,9 @@ h1 {
     }
     p{
       font-size: 13px;
+    }
+    .event{
+      padding: 15px 0px;
     }
 }
 @media only screen and (min-width: 350px) and (max-width: 480px){
@@ -159,7 +166,13 @@ h1 {
       padding-bottom:0;
       padding-left: 15px;
       padding-right: 10px;
-      }
+    }
+    table{
+      border-spacing: 0;
+      background-color: white;
+      margin: 4% 5.3%;
+      padding: 5% 0%;
+    }
     .time{
       padding-top:0;
       padding-bottom: 0;
@@ -176,7 +189,12 @@ h1 {
   h2 {
       font-size: 25px;
   }
-
+  table{
+    border-spacing: 0;
+    background-color: white;
+    margin: 4% 5.3%;
+    padding: 5% 10%;
+  }
   .scheduleEvent{
     padding-top:0;
     padding-bottom:0;
