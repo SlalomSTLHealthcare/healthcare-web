c6810d66b6e3c203117750a815ae152c2a860b75
Daniel Borstelmann <dborstelmann@wustl.edu>
2018-11-13 16:02:43 -0600

    remove registration

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -2,10 +2,14 @@
   <div class="registration">
       <h1>Registration</h1>
       <div class="decoration"></div>
-      <h3>HealthSTLX is about our community and creating a space for healthcare professionals to communicate, connect and create actionable solutions for key challenges in St. Louis. We look forward to connecting with you in November at this inaugural summit. Make sure to register below and consider an in-kind donation to United Way in lieu of a registration fee.</h3>
-      <h3>Your registration constitutes permission to use photos, audio, and video recording taken of you at the conference for promotional and educational purposes in connection with HealthSTLX by Slalom LLC.</h3>
-      <Registration class="registration-form"/>
-      <Login class="login" loginType='registration'/>
+      <!-- <h3>HealthSTLX is about our community and creating a space for healthcare professionals to communicate, connect and create actionable solutions for key challenges in St. Louis. We look forward to connecting with you in November at this inaugural summit. Make sure to register below and consider an in-kind donation to United Way in lieu of a registration fee.</h3> -->
+      <!-- <h3>Your registration constitutes permission to use photos, audio, and video recording taken of you at the conference for promotional and educational purposes in connection with HealthSTLX by Slalom LLC.</h3> -->
+
+      <h3>
+        Registration is now closed, please feel free to walk-in and register at the event!
+      </h3>
+      <!-- <Registration class="registration-form"/> -->
+      <!-- <Login class="login" loginType='registration'/> -->
   </div>
 </template>
 
@@ -48,7 +52,7 @@ h1 {
 }
 h3 {
   font-size: 16px;
-  font-weight: lighter;
+  font-weight: 400;
   color: #fff;
 }
 .decoration {
